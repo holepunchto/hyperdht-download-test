14a7288dc7139e074405123822e960256072622e
marco-hp <marco.dinh.hp@gmail.com>
2026-07-09 10:24:12 +0200

    bare

--- dht-client.js
+++ dht-client.js
@@ -1,36 +1,41 @@
+#!/usr/bin/env node
 const DHT = require('hyperdht')
 const idEnc = require('hypercore-id-encoding')
+const { command, arg } = require('paparam')
 
-const KEY = process.argv[2]
+const cmd = command(
+  'dht-client',
+  arg('<dht-public-key>', 'Public key to connect to'),
+  ({ args }) => {
+    const { dhtPublicKey } = args
 
-if (!KEY) {
-  console.error('Usage: node dht-client.js <dht-public-key>')
-  process.exit(1)
-}
+    const dht = new DHT()
+    const conn = dht.connect(idEnc.decode(dhtPublicKey))
 
-const dht = new DHT()
-const conn = dht.connect(idEnc.decode(KEY))
+    let received = 0
+    let interval
+    const start = Date.now()
 
-let received = 0
-let interval
-const start = Date.now()
+    conn.on('open', () => console.log('connected'))
 
-conn.on('open', () => console.log('connected'))
+    conn.on('data', (chunk) => {
+      received += chunk.length
+    })
 
-conn.on('data', (chunk) => {
-  received += chunk.length
-})
+    conn.on('error', (err) => {
+      console.error('DHT error:', err)
+    })
 
-conn.on('error', (err) => {
-  console.error('DHT error:', err)
-})
+    conn.on('close', () => {
+      console.log('connection closed')
+      clearInterval(interval)
+    })
 
-conn.on('close', () => {
-  console.log('connection closed')
-  clearInterval(interval)
-})
+    interval = setInterval(() => {
+      const secs = (Date.now() - start) / 1000
+      console.log(`received ${received} bytes in ${secs.toFixed(1)}s (${(received / secs / 1e6).toFixed(1)} MB/s)`)
+    }, 1000)
+  }
+)
 
-interval = setInterval(() => {
-  const secs = (Date.now() - start) / 1000
-  console.log(`received ${received} bytes in ${secs.toFixed(1)}s (${(received / secs / 1e6).toFixed(1)} MB/s)`)
-}, 1000)
+cmd.parse()
